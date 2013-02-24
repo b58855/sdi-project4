98f29b57ec15ddd86d2fa3e5737f477a1f124a6d
Evan <evan@Evans-MacBook-Pro.local>
2013-02-24 17:17:06 -0500

    phone and email

--- Combs_Evan_Assignment4.js
+++ Combs_Evan_Assignment4.js
@@ -1,1 +1,75 @@
-alert("JavaScript works!");
+var PhoneNumber = function(phoneNumber)
+{
+	if (phoneNumber.indexOf("-") === 3 && phoneNumber.lastIndexOf("-") === 7 && phoneNumber.length === 12)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	};
+};
+console.log(PhoneNumber("000-000-0000"));
+
+var EmailAddress = function(address)
+{
+	if (address.indexOf("@") < address.indexOf("."))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	};
+};
+console.log(EmailAddress("edcombs@gmail.com"));
+
+var URL = function(url)
+{
+
+};
+
+var TitleCase = function(string)
+{
+
+};
+
+var StringSeperators = function(string)
+{
+
+};
+
+var Decimal = function(number, decimals)
+{
+
+};
+
+var FuzzyMatch = function(number, percentage)
+{
+
+};
+
+var TimeDifference = function(time1, time2)
+{
+
+};
+
+var StringToNumber = function(string)
+{
+
+};
+
+var SmallestValue = function(array)
+{
+
+};
+
+var TotalValue = function(array)
+{
+
+};
+
+var ShortByKey = function(object, key)
+{
+
+};
